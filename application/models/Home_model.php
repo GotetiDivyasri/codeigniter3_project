89c9d28210e8cb53185d60db24d3640e135b9b52
DivyaSrinivas <91971172+GotetiDivyasri@users.noreply.github.com>
2025-07-31 16:39:41 +0530

    Update Home_model.php

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -4,6 +4,15 @@ class Home_model extends CI_Model
 {
     public function homeData()
     {
-        return "divya";
+        $data = [
+            'name' => 'divya',
+            'number' => '9848032919'
+        ];
+        return $data;
+    }
+
+    public function contact($data)
+    {
+        return $this->db->insert('contact_us', $data);
     }
 }
